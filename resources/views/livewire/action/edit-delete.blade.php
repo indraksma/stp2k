95a2040be95ea3f59e0da2b77a3b53866cb041a0
indraksma <45510960+indraksma@users.noreply.github.com>
2024-02-11 19:12:50 +0700

    Pelanggaran Page Fix

--- resources/views/livewire/action/edit-delete.blade.php
+++ resources/views/livewire/action/edit-delete.blade.php
@@ -1,7 +1,13 @@
 <div class="d-flex justify-content-start">
-    <button wire:click="$emit('edit', {{ $data->id }})" class="btn btn-sm btn-info"><i
-            class="fas fa-edit"></i></button>&nbsp;
+    @if (Auth::user()->hasRole(['guru']) && $data->user_id == Auth::user()->id)
+        <button wire:click="$emit('edit', {{ $data->id }})" class="btn btn-sm btn-info"><i
+                class="fas fa-edit"></i></button>&nbsp;
+    @else
+        -
+    @endif
     @if (Auth::user()->hasRole(['admin', 'waka', 'kesiswaan']))
+        <button wire:click="$emit('edit', {{ $data->id }})" class="btn btn-sm btn-info"><i
+                class="fas fa-edit"></i></button>&nbsp;
         <button wire:click="$emit('delete', {{ $data->id }})" class="btn btn-sm btn-danger"
             onclick="confirm('Are you sure to delete?') || event.stopImmediatePropagation()"><i
                 class="fas fa-trash"></i></button>
